e6ba79bc32ab53e2c612c7086ead8f392920de5d
Michael Thornes <zytekaron@gmail.com>
2020-05-13 17:02:06 -0700

    and collisions.js

--- math/index.js
+++ math/index.js
@@ -1,9 +1,10 @@
 module.exports = {
+    collisions: require('./collisions'),
     factorial: require('./factorial'),
     fibonacci: require('./fibonacci'),
     random: require('./random'),
     randomInt: require('./randomInt'),
+    root: require('./root'),
     secureRandom: require('./secureRandom'),
-    secureRandomInt: require('./secureRandomInt'),
-    root: require('./root')
+    secureRandomInt: require('./secureRandomInt')
 };
