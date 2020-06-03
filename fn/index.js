2479fafb0ea0d100f912b0ea67c63838303713c8
Michael Thornes <zytekaron@gmail.com>
2020-06-02 21:59:43 -0700

    fix fn/index.js - add conform, fuzzy, regexEscape

--- fn/index.js
+++ fn/index.js
@@ -1,10 +1,13 @@
 module.exports = {
+    conform: require('./conform'),
     constrain: require('./constrain'),
+    fuzzy: require('./fuzzy'),
     group: require('./group'),
     inspect: require('./inspect'),
     isEmpty: require('./isEmpty'),
     isIterable: require('./isIterable'),
     percent: require('./percent'),
+    regexEscape: require('./regexEscape'),
     sameType: require('./sameType'),
     type: require('./type')
 };
