c7d1b308fc7cfdd6d4226ff0d3eecfab4809c821
Michael Thornes <zytekaron@gmail.com>
2020-06-06 02:35:42 -0700

    add jvar.identity, jvar.nil

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
  * @module jvar
  */
 
-/**
- * index module
- * @type {{ Enum: module, Cherry: module, BitSet: module, fn: module, utility: module, math: module }}
- * @exports module:jvar/math
- */
 module.exports = {
     BitSet: require('./bitset'),
     Cherry: require('./cherry'),
     Enum: require('./enum'),
     fn: require('./fn'),
     math: require('./math'),
-    utility: require('./utility')
+    OneTimePad: require('./otp'),
+    Randomizer: require('./randomizer'),
+    utility: require('./utility'),
+
+    identity: (value) => value,
+    nil: () => null
 };
