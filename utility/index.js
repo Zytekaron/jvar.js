6fd3d87b2fed427ca1e70b78b834c98263e8542e
Michael Thornes <zytekaron@gmail.com>
2020-06-06 02:17:13 -0700

    update utility/index.js: replace automatic exports with manual exports

--- utility/index.js
+++ utility/index.js
@@ -1,7 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-
-for (const file of fs.readdirSync(__dirname)) {
-    if (file === 'index.js') continue;
-    module.exports[file.replace(/\.js$/, '')] = require(path.join(__dirname, file));
-}
+module.exports = {
+    ensure: require('./ensure'),
+    randomString: require('./randomString'),
+};
